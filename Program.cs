fc162f84fd3ed2a171adad46f836c7895cfa0c89
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-19 16:06:15 +0530

    Scaffold the command-line interface

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Show help message if no arguments were provided, and return early
+            if (args.Length == 0)
+            {
+                Console.WriteLine("ShowHelp");
+                return;
+            }
+
+            // Switch on the subcommand and dispatch the corresponding action
+            string command = args[0];
+            switch (command.ToLower())
+            {
+                case "list":
+                case "lists":
+                    Console.WriteLine("ShowLists");
+                    break;
+                case "show":
+                case "view":
+                    Console.WriteLine("ShowTask");
+                    break;
+                case "add":
+                case "create":
+                    Console.WriteLine("CreateTask");
+                    break;
+                case "complete":
+                case "strike":
+                case "done":
+                    Console.WriteLine("CompleteTask");
+                    break;
+                case "delete":
+                    Console.WriteLine("DeleteTask");
+                    break;
+                default:
+                    Console.WriteLine("ShowHelp");
+                    break;
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
