f677c105ad7ed1aaff2f6f69c832a3fac25f17e7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-19 20:19:19 +0530

    Setup the Microsoft Graph Authentication Flow
    Allows the user to "login" and retrieve the task-lists from Microsoft To Do using Microsoft Graph

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿class Program
+using Azure.Identity;
+using Microsoft.Graph;
+
+class Program
 {
-    static void Main(string[] args)
+    /// <summary>Microsoft Graph Client</summary>
+    static GraphServiceClient? client;
+
+    /// <summary>Application Client ID</summary>
+    const string CLIENT_ID = "2157a77b-da98-48e8-8240-2d26d1dbe0b4";
+
+    // ----
+    // MAIN
+    // ----
+
+    static async Task Main(string[] args)
     {
         try
         {
@@ -11,14 +24,16 @@
                 return;
             }
 
+            // Initialize Microsoft Graph Client and login as user
+            await InitMicrosoftGraph();
+
             // Switch on the subcommand and dispatch the corresponding action
             string command = args[0];
             switch (command.ToLower())
             {
                 case "list":
                 case "lists":
-                    Console.WriteLine("ShowLists");
-                    break;
+                    await ShowList(); break;
                 case "show":
                 case "view":
                     Console.WriteLine("ShowTask");
@@ -43,7 +58,39 @@
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine($"\n\nError:");
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    /// <summary>Initialize the Microsoft Graph Client and login as the user</summary>
+    static async Task InitMicrosoftGraph()
+    {
+        // Define the interactive browser credentials
+        var credentials = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
+        {
+            ClientId = CLIENT_ID,
+            TenantId = "common",
+            RedirectUri = new Uri("http://localhost"),
+        });
+
+        // Initialize the client
+        client = new GraphServiceClient(credentials, ["Tasks.ReadWrite", "User.Read"]);
+
+        // Get the user's profile
+        var me = await client.Me.GetAsync();
+        Console.WriteLine($"☑️ Logged in as {me?.DisplayName}");
+    }
+
+    static async Task ShowList()
+    {
+        // Get all the todo lists
+        var lists = await client!.Me.Todo.Lists.GetAsync();
+
+        Console.WriteLine("\n📋 Your Lists:\n");
+        foreach (var list in lists!.Value!)
+        {
+            Console.WriteLine($" - {list.Id}\t{list.DisplayName}");
         }
     }
 }
