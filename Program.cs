c96bf02e5f3a72cdfc50d6d9713dcf267add1968
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-19 22:43:25 +0530

    Cache the Auth Token for subsequent use
    Before this, the user had to re-authenticate each time the cli was invoked; which was annoying to say the least.
    
    See https://learn.microsoft.com/en-us/entra/msal/dotnet/acquiring-tokens/acquire-token-silently

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
-using Azure.Identity;
+// Library
 using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Microsoft.Kiota.Abstractions.Authentication;
 
 class Program
 {
+    /// <summary>Name of the application</summary>
+    static readonly string NAME = "mstodo-cli";
+
     /// <summary>Microsoft Graph Client</summary>
     static GraphServiceClient? client;
 
     /// <summary>Application Client ID</summary>
     const string CLIENT_ID = "2157a77b-da98-48e8-8240-2d26d1dbe0b4";
+
+    const string AUTHORITY = "https://login.microsoftonline.com/common";
+
+    /// <summary>Permission scopes requested by the application</summary>
+    static readonly string[] SCOPES = ["User.Read", "Tasks.ReadWrite"];
+
+    /// <summary>Path to the application data folder</summary>
+    static readonly string APP_DIR = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+        NAME
+    );
+
+    /// <summary>Path to the token cache file</summary>
+    static readonly string TOKEN_CACHE_PATH = Path.Combine(APP_DIR, ".token.bin");
 
     // ----
     // MAIN
@@ -66,20 +85,60 @@ class Program
     /// <summary>Initialize the Microsoft Graph Client and login as the user</summary>
     static async Task InitMicrosoftGraph()
     {
-        // Define the interactive browser credentials
-        var credentials = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
+        // Instantiate client application
+        var app = PublicClientApplicationBuilder.Create(CLIENT_ID)
+            .WithAuthority(AUTHORITY)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+        // Configure persistent token cache
+        app.UserTokenCache.SetBeforeAccess(args =>
         {
-            ClientId = CLIENT_ID,
-            TenantId = "common",
-            RedirectUri = new Uri("http://localhost"),
+            // Load the token from the cache file if it exists
+            if (File.Exists(TOKEN_CACHE_PATH))
+            {
+                args.TokenCache.DeserializeMsalV3(File.ReadAllBytes(TOKEN_CACHE_PATH));
+            }
+        });
+        app.UserTokenCache.SetAfterAccess(args =>
+        {
+            if (args.HasStateChanged)
+            {
+                // Create the app data folder if it does not exist yet
+                if (APP_DIR != null && !Directory.Exists(APP_DIR))
+                {
+                    Directory.CreateDirectory(APP_DIR);
+                }
+                // Save the token to the cache file
+                File.WriteAllBytes(TOKEN_CACHE_PATH, args.TokenCache.SerializeMsalV3());
+            }
         });
 
-        // Initialize the client
-        client = new GraphServiceClient(credentials, ["Tasks.ReadWrite", "User.Read"]);
+        // Acquire Token
+        AuthenticationResult result;
+        var accounts = await app.GetAccountsAsync();
+        try
+        {
+            // Try silent sign-in first...
+            result = await app.AcquireTokenSilent(SCOPES, accounts.FirstOrDefault()).ExecuteAsync();
+        }
+        catch (MsalUiRequiredException)
+        {
+            // ... otherwise, fallback to interactive browser flow
+            result = await app.AcquireTokenInteractive(SCOPES)
+                .WithPrompt(Prompt.SelectAccount)
+                .ExecuteAsync();
+        }
 
-        // Get the user's profile
-        var me = await client.Me.GetAsync();
-        Console.WriteLine($"☑️ Logged in as {me?.DisplayName}");
+        Console.WriteLine($"☑️ Logged in as user: {result.Account.Username}");
+
+        // Plug token in graph
+        var provider = new BaseBearerTokenAuthenticationProvider(
+            new TokenProvider(result.AccessToken)
+        );
+
+        // Instantiate the Microsoft Graph client
+        client = new GraphServiceClient(provider);
     }
 
     static async Task ShowList()
@@ -93,4 +152,21 @@ class Program
             Console.WriteLine($" - {list.Id}\t{list.DisplayName}");
         }
     }
+}
+
+// Simple wrapper class implementing IAccessTokenProvider
+class TokenProvider : IAccessTokenProvider
+{
+    private string _token;
+    public TokenProvider(string token) => _token = token;
+
+    public Task<string> GetAuthorizationTokenAsync(
+        Uri uri,
+        Dictionary<string, object>? additionalAuthenticationContext = null,
+        CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_token);
+    }
+
+    public AllowedHostsValidator AllowedHostsValidator => new();
 }
