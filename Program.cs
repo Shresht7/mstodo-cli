a1c973ac288f56022a54391593ad7919c7f376dd
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-21 22:53:56 +0530

    Improve the help message

--- Program.cs
+++ Program.cs
@@ -194,21 +194,29 @@ class Program
     /// <summary>Show the help message</summary>
     static void ShowHelp()
     {
-        Console.WriteLine($"Usage: {NAME} <command> [arguments] [--json]\n");
-        Console.WriteLine("A command-line-interface to interact with Microsoft To Do\n");
+        Console.WriteLine($"Usage: {NAME} <command> [arguments] [--json]");
+        Console.WriteLine("");
+
+        Console.WriteLine("A command-line-interface to interact with Microsoft To Do");
+        Console.WriteLine("");
+
+        Console.WriteLine("Global Options:");
+        Console.WriteLine("  --json      Output in JSON format");
+        Console.WriteLine("");
+
         Console.WriteLine("Commands:");
         Console.WriteLine("  login       Login with Microsoft Graph Services");
         Console.WriteLine("  logout      Logout from Microsoft Graph Services");
         Console.WriteLine("  user        Show current user information");
+        Console.WriteLine("");
+
         Console.WriteLine("  lists       Show your todo lists");
         Console.WriteLine("  show <list> [--limit <number>] Show tasks in a todo list");
         Console.WriteLine("  add         Add a new task");
         Console.WriteLine("  complete    Complete a task");
-        Console.WriteLine("  strike      Complete a task");
-        Console.WriteLine("  done        Complete a task");
         Console.WriteLine("  delete      Delete a task");
+        Console.WriteLine("");
+
         Console.WriteLine("  help        Show this help message");
-        Console.WriteLine("\nGlobal Options:");
-        Console.WriteLine("  --json      Output in JSON format");
     }
 }
