8ece1955bb546cbc64f5a882d7af5e7c40fb140e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-02 11:17:41 +0530

    Extract the `HelpCommand`

--- Program.cs
+++ Program.cs
@@ -69,8 +69,7 @@ class Program
                 case "--help":
                 case "-h":
                 default:
-                    ShowHelp();
-                    break;
+                    await new HelpCommand().ExecuteAsync(context); break;
 
             }
         }
@@ -79,37 +78,5 @@ class Program
             Console.WriteLine($"Error:");
             Console.WriteLine(ex.ToString());
         }
-    }
-
-    // COMMANDS
-    // --------
-
-    /// <summary>Show the help message</summary>
-    static void ShowHelp()
-    {
-        Console.WriteLine($"Usage: {NAME} <command> [arguments] [--json]");
-        Console.WriteLine("");
-
-        Console.WriteLine("A command-line-interface to interact with Microsoft To Do ☑️");
-        Console.WriteLine("");
-
-        Console.WriteLine("Global Options:");
-        Console.WriteLine("  --json      Output in JSON format");
-        Console.WriteLine("");
-
-        Console.WriteLine("Commands:");
-        Console.WriteLine("  login       Login with Microsoft Graph Services");
-        Console.WriteLine("  logout      Logout from Microsoft Graph Services");
-        Console.WriteLine("  user        Show current user information");
-        Console.WriteLine("");
-
-        Console.WriteLine("  lists                              Show your todo lists");
-        Console.WriteLine("  show <list> [--limit <number>]     Show tasks in a todo list");
-        Console.WriteLine("  add <list> <title>                 Add a new task to a specific list");
-        Console.WriteLine("  complete <list> <task>             Complete a task in a specific list");
-        Console.WriteLine("  delete <list> <task>               Delete a task in a specific list");
-        Console.WriteLine("");
-
-        Console.WriteLine("  help        Show this help message");
     }
 }
